2dd9cf9dc1748c38cf53f4943b478940e39346d4
asmoon-qiuqiu <2371824615@qq.com>
2025-10-15 21:10:56 +0800

    更新.main内容

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/', //根路径
+      name: 'home', //路由名称
+      component: () => import('../components/views/Home.vue')
+    }
   ],
 })
 
