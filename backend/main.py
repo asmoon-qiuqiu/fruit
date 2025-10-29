bb52965a8e11f23117eb5a48a1ea800bd072a58b
asmoon-qiuqiu <2371824615@qq.com>
2025-10-29 18:09:31 +0800

    修改backend

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
-from typing import Annotated
+from typing import Union
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
